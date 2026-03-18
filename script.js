e54214da336ec9dfc916f5a62e807039667af3a8
Ramon Q.Silva <ramonqrsilva@gmail.com>
2026-03-18 16:16:51 -0300

    integração do HTML com JavaScript
    Conecta os elementos do formulário com a função de cálculo, permitindo interação e exibição dinâmica do resultado na interface.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.getElementById("imcForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const peso = parseFloat(document.getElementById("peso").value);
+  const altura = parseFloat(document.getElementById("altura").value);
+  const imc = calcularIMC(peso, altura);
+
+  document.getElementById("resultado").innerText = `IMC: ${imc}`;
+});
+
+
 function calcularIMC(peso, altura) {
   return (peso / (altura * altura)).toFixed(2);
 }
