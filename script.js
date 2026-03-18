c5b7d57c4ed13783df25f8d8c5817a6bae07eb01
Ramon Q.Silva <ramonqrsilva@gmail.com>
2026-03-18 16:26:15 -0300

    adição de classificação de peso do IMC
    Implementa lógica para classificar o IMC em categorias como baixo peso, normal, sobrepeso e obesidade.

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ document.getElementById("imcForm").addEventListener("submit", function(event) {
   const altura = parseFloat(document.getElementById("altura").value);
   const imc = calcularIMC(peso, altura);
 
-  document.getElementById("resultado").innerText = `IMC: ${imc}`;
+  document.getElementById("resultado").innerText = 
+  `IMC: ${imc} - ${classificarIMC(imc)}`;
 });
-
 
 function calcularIMC(peso, altura) {
   return (peso / (altura * altura)).toFixed(2);
+}
+
+function classificarIMC(imc) {
+  if (imc < 18.5) return "Baixo peso";
+  else if (imc >= 18.5 && imc <= 24.9) "";
+  else if (imc >= 25 && imc <= 29.9) return "Sobrepeso";
+  else if (imc >= 30 && imc <= 34.9) return "";
+  else if (imc >= 35 && imc <= 39.9) return "Obesidade grau II";
+  else return "Obesidade grau III";
 }
